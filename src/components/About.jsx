b202f3c3093ae79bd3aa710a14a91728b39fbebe
Yousef0Saleh <yousef.programmer100@gmail.com>
2024-09-23 00:58:36 +0300

    Your commit message here

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+
+    <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a highly skilled front end developer with over 5 years of experience to deliver exceptional responsive and user friendly user interfaces using latest technologies. I am well versed in HTML, CSS, JavaScript and can create responsive SPA web applications that are performant and scalable. Experienced with ReactJS, I write modularized and scalable UIs that is driven by components, aided with Tailwind CSS to craft reusable, human-readable designs. Besides that, I use Bootstrap and jQuery to quickly design the look and create interactive functionality so that my site can work on any device or browser as well.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, 'about')
---
+++ src/constants/index.js
@@ -0,0 +1,235 @@
+/* eslint-disable no-unused-vars */
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  xora,
+  iphone,
+  tripguide,
+  threejs,
+  github,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "ReactJs Developer",
+    icon: mobile,
+  },
+  {
+    title: "Responsive Design Specialist",
+    icon: backend,
+  },
+  {
+    title: "UI/UX Designer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+];
+
+const experiences = [
+  {
+    title: "Web Developer",
+    company_name: "Freelance / Personal Projects",
+    icon: web, // Placeholder for icon
+    iconBg: "#383E56",
+    date: "From 2018",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with designers and developers to create user-friendly interfaces.",
+      "Implementing responsive design and ensuring cross-browser compatibility across projects.",
+      "Participating in self-led code reviews to enhance code quality and efficiency.",
+    ],
+  },
+  {
+    title: "React.js Developer",
+    company_name: "Self-initiated Projects",
+    icon: reactjs, // Placeholder for icon
+    iconBg: "#383E56",
+    date: "From 2019",
+    points: [
+      "Building dynamic and interactive web applications using React.js.",
+      "Utilizing state management libraries like Redux for efficient data handling.",
+      "Optimizing components for maximum performance across a variety of web-capable devices.",
+      "Engaging in collaborative project work to refine UI and enhance user experience.",
+    ],
+  },
+  {
+    title: "UI/UX Designer",
+    company_name: "Personal Projects",
+    icon: creator, // Placeholder for icon
+    iconBg: "#383E56",
+    date: "From 2020",
+    points: [
+      "Designing wireframes and prototypes for web applications to improve user experience.",
+      "Conducting user research to inform design decisions and enhance usability.",
+      "Collaborating with developers to ensure design fidelity during implementation.",
+      "Iterating on designs based on user feedback and testing results.",
+    ],
+  },
+  {
+    title: "Code Quality Advocate",
+    company_name: "Independent Work",
+    icon: github, // Placeholder for icon
+    iconBg: "#383E56",
+    date: "From 2018",
+    points: [
+      "Establishing coding standards and best practices for personal projects.",
+      "Reviewing and refactoring code to improve readability and performance.",
+      "Documenting processes and solutions for future reference.",
+      "Mentoring peers in effective coding techniques and practices.",
+    ],
+  },
+];
+
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Xora",
+    description:
+      "A modern template for an AI video editing website, designed to enhance user experience with seamless editing tools and intuitive navigation.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "SaaS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: xora,
+    source_code_link: "https://xora-project.vercel.app/",
+  },
+  {
+    name: "IPhone",
+    description:
+      "iPhone Project Website is an enhanced version of the official iPhone site, showcasing a modern design and improved performance while maintaining the brand's elegance.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "threejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tialwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: iphone,
+    source_code_link: "https://project-iphone.vercel.app/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
